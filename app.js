0c0c0ee5b662252f945cdc0e2cf6babbe23d4688
Parker Hemming <parkerhemming2001@gmail.com>
2023-04-21 19:14:28 -0600

    Testing

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8888;
+
+app.use(express.static('public'));
+app.use('/master', express.static(__dirname + '/public/master'));
+app.use('/assets', express.static(__dirname + '/public/assets'));
+app.use('/routes', express.static(__dirname + '/public/routes'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/routes/home/index.html");
+});
+
+app.listen(port, () => {
+    console.log(`App available at http://localhost:${port}`);
+});
